b8f969b433c3960b763ae65fdc3df37bcd7bca62
GOLDMAN <mihalanius@gmail.com>
2026-09-06 05:19:47 +0300

    add mascot.png and icon to build, update build.bat

--- build.bat
+++ build.bat
@@ -20,7 +20,7 @@ echo [2/5] Building executable...
 cd src
 if exist dist rmdir /S /Q dist
 if exist build rmdir /S /Q build
-..\venv\Scripts\python.exe -m PyInstaller --onefile --windowed --name TalkerBox --add-data "../sounds;sounds" --add-data "../ads.json;." --add-data "hotkey_listener.py;." --add-data "../talkerbox.png;." --add-data "../help.html;." main.py
+..\venv\Scripts\python.exe -m PyInstaller --onefile --windowed --name TalkerBox --add-data "../sounds;sounds" --add-data "../ads.json;." --add-data "hotkey_listener.py;." --add-data "../talkerbox.png;." --add-data "../mascot.png;." --add-data "../help.html;." main.py
 cd ..
 
 echo.
@@ -30,6 +30,7 @@ copy src\dist\TalkerBox.exe dist\TalkerBox\
 copy ads.json dist\TalkerBox\
 xcopy /E /I /Y sounds dist\TalkerBox\sounds
 copy talkerbox.png dist\TalkerBox\
+copy mascot.png dist\TalkerBox\
 copy help.html dist\TalkerBox\
 
 echo.
